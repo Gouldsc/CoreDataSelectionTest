e0e3fcd419b28459e2be0176d63c00d83ca1ef19
Gouldsc <1923247+Gouldsc@users.noreply.github.com>
2021-08-30 17:28:19 -0400

    Initial Commit

---
+++ CoreDataSelectionTest/CoreDataSelectionTestApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoreDataSelectionTestApp.swift
+//  CoreDataSelectionTest
+//
+//  Created by Scott Gould on 8/30/21.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataSelectionTestApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
