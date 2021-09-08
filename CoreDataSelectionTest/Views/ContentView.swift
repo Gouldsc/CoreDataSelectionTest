8143c524626cd3923fa7122ccc6e304dfdb3afa5
Gouldsc <1923247+Gouldsc@users.noreply.github.com>
2021-09-08 11:55:43 -0400

    Drag reordering of sourceList should be complete.

--- CoreDataSelectionTest/Views/ContentView.swift
+++ CoreDataSelectionTest/Views/ContentView.swift
@@ -5,7 +5,6 @@
 //  Created by Scott Gould on 8/30/21.
 //
 
-//	TODO: Drag reordering for SourceList
 //	TODO: Multiple Selection for SourceList
 //	TODO: Delete menu command for multipleSelection
 //	TODO: Implement the onHover "action" button contextMenu funcitonality with single click -- Default Menu macOS implementation uses popup menu which we don't want. See Notes.app for desired implementation
